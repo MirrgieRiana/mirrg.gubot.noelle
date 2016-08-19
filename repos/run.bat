8cadbf6b88d19578b9dd11e6a786ef4ec5d2465d
Mirrgie Riana <tacticsrealize2@gmail.com>
2016-08-19 09:53:21 +0900

    version: 0.0.3

--- repos/run.bat
+++ repos/run.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
 cd ..\mirrg.gubot.noelle
-javaw -jar ..\repos\mirrg.gubot.noelle-0.0.2.jar mirrg.gubot.noelle.GUNoelle
+javaw -jar ..\repos\mirrg.gubot.noelle-0.0.3.jar mirrg.gubot.noelle.GUNoelle
