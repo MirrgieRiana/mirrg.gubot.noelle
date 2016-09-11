bd3ae0a8c04e991ad190e7f469132136cf98229d
Mirrgie Riana <tacticsrealize2@gmail.com>
2016-09-12 03:14:15 +0900

    add: PluginSearchGroup

---
+++ mirrg.gubot.noelle/src/main/java/mirrg/gubot/noelle/pluginsearch/PluginSearchGroup.java
@@ -0,0 +1,56 @@
+package mirrg.gubot.noelle.pluginsearch;
+
+import java.util.ArrayList;
+
+import mirrg.helium.standard.hydrogen.struct.Tuple;
+
+public class PluginSearchGroup implements IPluginSearch
+{
+
+	private ArrayList<IPluginSearch> plugins = new ArrayList<>();
+
+	public void add(IPluginSearch plugin)
+	{
+		plugins.add(plugin);
+	}
+
+	@Override
+	public Tuple<EnumPluginSearchCondition, String> tick(int milis)
+	{
+		boolean skippable = true;
+		for (IPluginSearch plugin : plugins) {
+			Tuple<EnumPluginSearchCondition, String> res = plugin.tick(milis);
+
+			if (res.getX() == EnumPluginSearchCondition.STOP) return res;
+
+			if (res.getX() == EnumPluginSearchCondition.WAITING) {
+				skippable = false;
+			}
+
+		}
+		if (skippable) {
+			return new Tuple<>(EnumPluginSearchCondition.SKIPPABLE, null);
+		} else {
+			return new Tuple<>(EnumPluginSearchCondition.WAITING, null);
+		}
+	}
+
+	@Override
+	public void onSkipped()
+	{
+		plugins.forEach(IPluginSearch::onSkipped);
+	}
+
+	@Override
+	public void onStarted()
+	{
+		plugins.forEach(IPluginSearch::onStarted);
+	}
+
+	@Override
+	public void openDialog()
+	{
+		plugins.forEach(IPluginSearch::openDialog);
+	}
+
+}
